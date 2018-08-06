5aef433fb211aaa8b2d451b170d71b2ce00b0f2e
kiran <kiran.valluri@gmail.com>
2018-08-06 12:00:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/home/paypalintegration/PayPalConfig.java
@@ -0,0 +1,10 @@
+package com.example.home.paypalintegration;
+
+/**
+ * Created by Belal on 5/1/2016.
+ */
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AVZUbOX3ry-gyvTBVykh9TnK1v49hM0ycQiquryr8NjuRwnayplCFEm1M4ZnK5Q9JCcWzn5_briWUeRH";
+
+}
